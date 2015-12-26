0fc660a3f1e95f78afb6dcfd18248438b6f4fc2f
Sean <sean8694@gmail.com>
2015-12-27 02:20:10 +0800

    change to test config

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -4,17 +4,17 @@ return array(
 	'TMPL_L_DELIM'=>'<{', //修改左定界符
 	'TMPL_R_DELIM'=>'}>', //修改右定界符
 	'SHOW_PAGE_TRACE'=>false,//开启页面Trace
-	'FRUIT_APP_URL'	=> 'http://izhier.com/', // 应用目录
-    'FRUIT_APP_URL_DE'	=> 'izhier.com', // 应用目录
+	'FRUIT_APP_URL'	=> 'http://sean.ngrok.natapp.cn/', // 应用目录
+    'FRUIT_APP_URL_DE'	=> 'sean.ngrok.natapp.cn', // 应用目录
 	// 微信参数
-	'WEICHAT_APPID'	=> 'wx659ec57ec2da708e',
-	'WEICHAT_SECRET'=> '32eb64709524ea9eb8d0c9dd8b4a63d0',
+	'WEICHAT_APPID'	=> 'wxb057ae600fdf9f41',
+	'WEICHAT_SECRET'=> '9077a0a0302ebc3fdebe5a520973ee39',
 	// 数据库
 	'DB_TYPE'=>'mysql', //设置数据库类型
-	'DB_HOST'=>'localhost',//设置主机
+	'DB_HOST'=>'127.0.0.1',//设置主机
 	'DB_NAME'=>'zhier',//设置数据库名
-	'DB_USER'=>'root', //设置用户名
-	'DB_PWD'=>'zhi*er@o1W', //设置密码
+	'DB_USER'=>'zhier', //设置用户名
+	'DB_PWD'=>'123', //设置密码
 	'DB_PORT'=>'3306', //设置端口号
 	'DB_PREFIX'=>'', //设置表前缀
 	'SESSION_EXPIRE'        =>7200,
@@ -33,19 +33,19 @@ return array(
 	// 管理后台-展示图片位置
 	'ADMIN_IMG'		=> '/var/www/guozhi/Public/upload/product/',
 	// 管理后台-登录
-	'ADMIN_LOGIN'	=> 'http://izhier.com/index.php/Admin/Admin/',
+	'ADMIN_LOGIN'	=> 'http://sean.ngrok.natapp.cn/index.php/Admin/Admin/',
 	// 管理后台-地址
-	'ADMIN_INDEX'	=> 'http://izhier.com/index.php/Admin/Product/',
+	'ADMIN_INDEX'	=> 'http://sean.ngrok.natapp.cn/index.php/Admin/Product/',
 	// 管理后台-意见地址
-	'ADMIN_OPINION'	=> 'http://izhier.com/index.php/Admin/Opinion/',
+	'ADMIN_OPINION'	=> 'http://sean.ngrok.natapp.cn/index.php/Admin/Opinion/',
 	// 管理后台-意见地址
-	'ADMIN_RECOMMEND'	=> 'http://izhier.com/index.php/Admin/Recommend/',
+	'ADMIN_RECOMMEND'	=> 'http://sean.ngrok.natapp.cn/index.php/Admin/Recommend/',
 	// 管理后台-意见状态
 	'OPINION_STATUS'=> array(0=>array('status'=>0,'statusname'=>'未回复'),1=>array('status'=>1,'statusname'=>'已回复')),
 	// 管理后台-订单评论
 	'ORDER_COMMEND_STATUS'=> array(1=>array('status'=>1,'statusname'=>'未回'),2=>array('status'=>2,'statusname'=>'隐藏'),3=>array('status'=>3,'statusname'=>'已回'),0=>array('status'=>0,'statusname'=>'未评'),4=>array('status'=>4,'statusname'=>'不回')),
 	// 个人中心-我的钱包
-	'HOME_COUPON'=> 'http://izhier.com/index.php/Home/Index/wallet',
+	'HOME_COUPON'=> 'http://sean.ngrok.natapp.cn/index.php/Home/Index/wallet',
 	// 整体折扣
 	'GLOBAL_DISCOUNT' => ['DISCOUNT'=>0.8,'REASION'=>'开业庆典，全场8折优惠'],
 	// 店铺休息时间
